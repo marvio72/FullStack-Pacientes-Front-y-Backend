1b23e6a641f327a96a6a80df10c11195d663def9
marvio72 <mruvalcaba@oversistemas.com>
2021-03-23 15:30:05 -0600

    Creando el Formulario para nuevas citas

--- frontend/src/components/Pacientes.js
+++ frontend/src/components/Pacientes.js
@@ -1,17 +1,18 @@
 import React, { Fragment } from 'react';
+import { Link } from 'react-router-dom';
 
 const Pacientes = ({ citas }) => {
   if (citas.lenght === 0) return null;
 
   return (
     <Fragment>
-      <h1 className="my-5">Administradr de Pacientes</h1>
+      <h1 className="my-5">Administrador de Pacientes</h1>
       <div className="container mt-5 py-5">
         <div className="row">
           <div className="col-12 mb-5 d-flex justify-content-center">
-            <a href="#" className="btn btn-success text-uppercase py-2 px-5 font-weight-bold">
+            <Link to={'/nueva'} className="btn btn-success text-uppercase py-2 px-5 font-weight-bold">
               Crear Cita
-            </a>
+            </Link>
           </div>
           <div className="col-md-8 mx-auto">
             <div className="list-group">
